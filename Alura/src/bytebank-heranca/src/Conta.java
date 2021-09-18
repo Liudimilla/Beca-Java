f95d8d25073792616f4e680bdf56add8a3255b12
Liudimilla Silva <42663858+Liudimilla@users.noreply.github.com>
2021-09-17 21:46:50 -0300

    Parte 5

---
+++ Alura/src/bytebank-heranca/src/Conta.java
@@ -0,0 +1,97 @@
+public class Conta {
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+    private static int total = 0;// estancia este atributo para esta classe
+     
+  //construtor a requisição da variável
+    public Conta( int agencia, int numero) { 
+    	Conta.total ++;
+    	System.out.println("O total de contas e "+ Conta.total);
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = 100;
+        System.out.println("estou criando uma conta " + this.numero);
+    }
+    
+    
+    public void deposito(double valor) {
+    	this.saldo = this.saldo + valor;
+    }
+    //Metodos .:
+    public boolean transfere(double valor, Conta destino) { 
+        if(this.saldo >= valor) { 
+            this.saldo -= valor;
+            destino.deposito(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean ted(double valor, Conta destino) {
+    	if(this.saldo >= valor) {
+    		this.saldo -= valor;
+    		destino.deposito(valor);
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+
+    	public boolean saca(double valor) { // Metodo 2.:
+    	    if(this.saldo >= valor) { 
+    	    	this.saldo -= valor;//tira valor dele menos 
+    	   
+    	        return true;
+    	
+    	    } else {
+    	    	 return true;
+    	    }
+    	}
+    	    // metodo Pega Saldo que esta em forma PRIVADA
+    	    public double getSaldo() {
+    	    	return this.saldo;
+    	    }
+
+    public int getNumero() { 
+        return this.numero;
+    }
+
+    public void setNumero(int numero) { 
+    	if (numero <= 0) {
+            System.out.println("não pode valor <= 0");
+            return;
+        }
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+    
+    public void setAgencia(int agencia) { 
+        if (agencia <= 0) { 
+            System.out.println("nao pode valor menor igual a 0");
+            return;
+        }
+        this.agencia = agencia;            
+    }
+    public void setTitular(Cliente titular) { 
+        this.titular = titular; 
+
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+//método da classe, que nos retorna o total das contas, com um 
+    //atributo estático privado, o nome da classe:
+
+	public static int getTotal() { //método estático gette
+		return Conta.total;
+	}
+    
+    
+}
