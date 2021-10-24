9b16fbfe8d94e623c6a83a43482626daf40c6bde
Liudimilla Silva <42663858+Liudimilla@users.noreply.github.com>
2021-10-23 22:26:21 -0300

    Autenticaçao via JWT
    Recuperando o token, 
    Validando o token, 
    Autenticando o cliente via Spring Security

---
+++ Alura/src/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,56 @@
+package br.com.alura.forum.config.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
+//OncePerRequestFilter; filtro do spring puxar uma unica vez.
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+	
+	private TokenService tokenService;
+	private UsuarioRepository repository;//para pegar dados do cliente no banco de dados
+
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository repository) {
+		this.tokenService = tokenService;
+		this.repository = repository;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = recuperarToken(request);
+		boolean valido = tokenService.isTokenValido(token);//para verificar se o cliente esta colocando O token correto.
+		if (valido) {
+			autenticarCliente(token);
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+
+	private void autenticarCliente(String token) {//Metodo para liberar autenticaçao apois ser validado
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = repository.findById(idUsuario).get();
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");//nome "Authorization", que esta no cabelho do postmam
+		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {//para verificar o campo do cabeçalho
+			return null;
+		}
+		
+		return token.substring(7, token.length());
+	}
+
+}
