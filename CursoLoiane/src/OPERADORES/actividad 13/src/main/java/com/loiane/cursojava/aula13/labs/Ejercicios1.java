c0fa4c7436160793e936b8ecf07496a1e5cfdf68
Liudimilla Silva <42663858+Liudimilla@users.noreply.github.com>
2022-06-07 18:53:06 -0300

    Delete CursoLoiane/src/OPERADORES/actividad 13 directory

--- CursoLoiane/src/OPERADORES/actividad 13/src/main/java/com/loiane/cursojava/aula13/labs/Ejercicios1.java
+++
@@ -1,9 +0,0 @@
-package com.loiane.cursojava.aula13.labs;
-
-public class Ejercicios1 {
-    public static void main (String[] args) {
-        System.out.println("Ola mundo");
-        System.out.print("Seja bem vindo");
-    }
-
-}
